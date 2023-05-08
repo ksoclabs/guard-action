8ba316b236038cf1c42c0976cd2f00c47f5e3043
Pawel Kowalak <pawel@ksoc.com>
2023-05-08 13:28:36 +0200

    Support for SARIF output format
    Signed-off-by: Pawel Kowalak <pawel@ksoc.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh -l
 
 EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
-echo "results<<$EOF" >> $GITHUB_OUTPUT
-/app/policy-executor policies execute >> $GITHUB_OUTPUT
-exit_code=$?
-echo "$EOF" >> $GITHUB_OUTPUT
-cat $GITHUB_OUTPUT
+if [ $FORMAT = "sarif" ]; then
+  SARIF_OUTPUT_FILE_NAME="./report.sarif"
+  /app/policy-executor policies execute > $SARIF_OUTPUT_FILE_NAME
+  exit_code=$?
+  echo "sarif=$SARIF_OUTPUT_FILE_NAME" >> $GITHUB_OUTPUT
+else
+  echo "results<<$EOF" >> $GITHUB_OUTPUT
+  /app/policy-executor policies execute >> $GITHUB_OUTPUT
+  exit_code=$?
+  echo "$EOF" >> $GITHUB_OUTPUT
+fi
 exit $exit_code
